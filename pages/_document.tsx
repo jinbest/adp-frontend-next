abc6deb541c8993cb9a7fbed9d37765100a02be4
Ehidiamen Osarhemen <eosarhemen@gmail.com>
2021-04-21 20:28:37 -0500

    Revert "Use getServerSideProps instead of getInitProps to get subDomain from URL"
    This reverts commit 351fb6b83c0b2c34ceb2b5126dca97789deaf733.

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,5 +1,6 @@
 import React from "react"
 import Document, { Html, Head, Main, NextScript } from "next/document"
+
 export default class CustomDocument extends Document {
   render() {
     return (
