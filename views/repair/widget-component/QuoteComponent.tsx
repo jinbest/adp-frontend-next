2201db8e0c7fefb4103ed61b932068c4ffa32959
jinbest <jin.bestvictoria718@gmail.com>
2021-05-11 06:53:37 +0800

    fixed some functionality issue for repair widget fixed

--- views/repair/widget-component/QuoteComponent.tsx
+++ views/repair/widget-component/QuoteComponent.tsx
@@ -126,7 +126,7 @@ const QuoteComponent = ({ data, quoteKey, repairWidgetData, code }: Props) => {
                   {repairWidgetData.contactDetails.address1.name}
                 </Typography>
               )}
-              {code !== "MAIL_IN" && (
+              {code !== "MAIL_IN" && quoteKey === 1 && (
                 <Typography className="details">
                   {repairWidgetData.bookData[code].week +
                     ", " +
