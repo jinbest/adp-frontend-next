0f7dd3ae5d6ad74762bbe5921a92b2eb88918b8d
jindev.718 <jin.bestvictoria718@gmail.com>
2022-07-16 01:23:05 +0800

    service card updated

--- components/ServiceCard.tsx
+++ components/ServiceCard.tsx
@@ -1,8 +1,8 @@
-/* eslint-disable @typescript-eslint/no-non-null-assertion */
 import React from "react"
 import Button from "./Button"
 import { storesDetails } from "../store"
-import { useRouter } from "next/router"
+import { Link } from "react-router-dom"
+import { isExternal } from "../services/helper"
 
 type ServiceCardType = {
   icon?: string
@@ -11,31 +11,54 @@ type ServiceCardType = {
   link?: string
 }
 
+type WrapperType = {
+  link?: string
+  children?: React.ReactNode
+}
+
+const Wrapper: React.FC<WrapperType> = ({ link, children }) => {
+  if (!link) return <React.Fragment>{children}</React.Fragment>
+  else {
+    if (isExternal(link)) {
+      return (
+        <a href={link} target="_blank" rel="noreferrer" style={{textDecoration: "none"}}>
+          {children}
+        </a>
+      )
+    } else {
+      return (
+        <Link to={link} style={{textDecoration: "none"}}>{children}</Link>
+      )
+    }
+  }
+}
+
 const ServiceCard: React.FC<ServiceCardType> = (props) => {
   const { icon, title, btnTitle, link } = props
   const data = storesDetails.storeCnts
   const themeType = data.general.themeType
-  const router = useRouter()
+
   return (
-    <div className="service-card" onClick={() => router.push(link!)}>
-      <img src={icon} alt="icon" />
-      <div className="service-content">
-        <div className="service-title">{title}</div>
-        {btnTitle &&
-          <Button
-            margin="33px auto 0"
-            borderR="0"
-            fontSize="20px"
-            height="48px"
-            border="none"
-            title={btnTitle}
-            fontFamily={themeType === "marnics" ? "Helvetica Neue Bold" : "Poppins Regular"}
-            onClick={() => router.push(link!)}
-            bgcolor={data.general.colorPalle.cardBtnColor}
-          />
-        }
+    <Wrapper link={link}>
+      <div className="service-card">
+        <img src={icon} alt="icon" />
+        <div className="service-content">
+          <div className="service-title">{title}</div>
+          {btnTitle &&
+            <Button
+              margin="33px auto 0"
+              borderR="0"
+              fontSize="20px"
+              height="48px"
+              border="none"
+              title={btnTitle}
+              fontFamily={themeType === "marnics" ? "Helvetica Neue Bold" : "Poppins Regular"}
+              bgcolor={data.general.colorPalle.cardBtnColor}
+            />
+          }
+        </div>
       </div>
-    </div>
+    </Wrapper>
   )
 }
 export default ServiceCard
